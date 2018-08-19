1ebe257f8705be8cba06bde767f433b854527df0
willh99 <willh99@gwmail.gwu.edu>
2018-08-19 20:09:22 +0000

    Optional persistance and new tests

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-     log_in user
-     remember user
-     redirect_to user
+      log_in user
+     
+      # If Remember_Me box is checked, remember session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -54,4 +54,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?('')
   end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+  
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
