356d1804f8a8c5278543da5dbbfb2c3e88abc90f
willh99 <willh99@gwmail.gwu.edu>
2018-08-20 13:06:48 +0000

    settings page created

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     # Returns the hash digest of the given string
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+                                                      BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
     end
     
