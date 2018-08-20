ee6edc98b6fd6a5ddaec46a0a67246c7e120ea1d
willh99 <willh99@gwmail.gwu.edu>
2018-08-20 20:00:15 +0000

    Users page complete

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -40,8 +40,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
   
-  test "should redirect when wrong user" do
-    
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
   end
 
 end
